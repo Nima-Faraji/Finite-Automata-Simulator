a3419148a2130f6b92ba5ec31abcc9b13205e376
Tony Amin <TonyAmin2520@gmail.com>
2020-08-01 11:40:42 -0700

    saving

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -15,10 +19,24 @@ public class Controller  {
 	Stage infoWindow;
 	
 	public Button close;
+	public Button about;
 	
 	public void handleButtonClick() {
-		System.out.println("you have exited the application");
-	
-		System.exit(0);
+		
+//		if(close.isArmed())
+//		{
+			System.out.println("you have exited the application");
+			System.exit(0);
+//		}
+//		else if(about.isPressed()) {
+//			System.out.println("you have selected to know more about the application");
+//		}
+//		
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
